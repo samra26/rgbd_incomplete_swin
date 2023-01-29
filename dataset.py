230977874b41b280e5b3fa2eba99741cc29f0f89
samra26 <75056386+samra26@users.noreply.github.com>
2023-01-30 00:29:59 +0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -34,7 +34,7 @@ class DatasetGenerate(Dataset):
 
         image = torch.Tensor(sal_image)
         mask = torch.Tensor(sal_label)
- 
+        print('duts',image.shape,mask.shape)
  
         #sample1 = {'rgb_image': image,'rgb_label': mask}
         return image,mask
@@ -74,6 +74,7 @@ class ImageDataTrain(data.Dataset):
         sal_label = torch.Tensor(sal_label)
         sal_edge = torch.Tensor(sal_edge)
         dq=depth_quality_score(sal_depth)
+        print('rgbd',sal_image.shape,sal_depth.shape,sal_label.shape)
         #sample = {'sal_image': sal_image, 'sal_depth': sal_depth, 'sal_label': sal_label, 'sal_edge': sal_edge,'depth_quality_score':dq,'name': self.sal_list[item % self.sal_num].split()[0].split('/')[1]}
         return sal_image,sal_depth,sal_label
 
